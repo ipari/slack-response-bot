5eb1cf6dd5909768755378c98c8fab75115fd581
Lee Kwangyoung <me@ipari.dev>
2019-03-07 01:34:51 +0900

    make functions into class

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import threading
-import time
-
 from flask import Flask
 from slackclient import SlackClient
 
-from bot import Brain
+from bot import Bot
 
 
 app = Flask(__name__)
@@ -19,48 +17,6 @@ def hello():
 @app.route("/main/")
 def main():
     return "Main"
-
-
-class Bot(object):
-    def __init__(self, sc):
-        self.sc = sc
-        self.brain = Brain()
-
-    @staticmethod
-    def is_message(response):
-        return response['type'] == 'message' and 'text' in response
-
-    @staticmethod
-    def is_from_bot(response):
-        return 'bot_id' in response
-
-    def say(self, response):
-        answer = self.brain.find_answer(response['text'])
-        if answer is None:
-            return
-
-        kwargs = {}
-        # Required
-        kwargs['channel'] = response['channel']
-        kwargs['text'] = answer
-        # Optional
-        kwargs['as_user'] = True
-        if 'thread_ts' in response:
-            kwargs['thread_ts'] = response['thread_ts']
-
-        self.sc.api_call('chat.postMessage', **kwargs)
-
-    def listen(self):
-        while True:
-            responses = sc.rtm_read()
-            for response in responses:
-                try:
-                    if not self.is_message(response) or self.is_from_bot(response):
-                        continue
-                    self.say(response)
-                except:
-                    pass
-            time.sleep(1)
 
 
 SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
--- bot.py
+++ bot.py
@@ -2,106 +2,136 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import codecs
-import os
 import random
 import time
 import yaml
 
 from konlpy.tag import Okt
-from slackclient import SlackClient
+
+__all__ = [b'Bot', b'Brain']
 
 
-__all__ = [b'Brain']
+class Bot(object):
+    def __init__(self, sc):
+        self.sc = sc
+        self.brain = Brain()
+
+    @staticmethod
+    def is_message(response):
+        return response['type'] == 'message' and 'text' in response
+
+    @staticmethod
+    def is_from_bot(response):
+        return 'bot_id' in response
+
+    def say(self, response):
+        answer = self.brain.find_answer(response['text'])
+        if answer is None:
+            return
+
+        kwargs = {}
+        # Required
+        kwargs['channel'] = response['channel']
+        kwargs['text'] = answer
+        # Optional
+        kwargs['as_user'] = True
+        if 'thread_ts' in response:
+            kwargs['thread_ts'] = response['thread_ts']
+
+        self.sc.api_call('chat.postMessage', **kwargs)
+
+    def listen(self):
+        while True:
+            responses = self.sc.rtm_read()
+            for response in responses:
+                try:
+                    if not self.is_message(response) or self.is_from_bot(response):
+                        continue
+                    self.say(response)
+                except:
+                    pass
+            time.sleep(1)
 
 
 class Brain(object):
 
-    _keywords = None
-
     def __init__(self):
-        self.twitter = Okt()
+        self.data = None
+        self.groups = {}
+        self.triggers = None
+        self.qnas = []
 
-    def find_answer(self, text):
-        return text
+        self.load_data()
+        self.okt = Okt()
 
-    # def keywords(self, update=False):
-    #     if update or self._keywords is None:
-    #
-    #
-    # def process_keywords(keywords, allow_group=True):
-    #     def process_group(keyword):
-    #         if keyword.startswith('group:'):
-    #             if allow_group:
-    #                 group_key = keyword.split(':')[1]
-    #                 try:
-    #                     return data['groups'][group_key]
-    #                 except KeyError:
-    #                     print('groups에서 {}를 찾을 수 없습니다.'.format(group_key))
-    #                     raise
-    #             print('groups: 는 conditions의 keywords 에만 사용할 수 있습니다.')
-    #             raise
-    #         return keyword
-    #
-    #     if not isinstance(keywords, list):
-    #         keywords = keywords.split(',')
-    #         keywords = [keyword.strip() for keyword in keywords]
-    #     keywords = [process_group(keyword) for keyword in keywords]
-    #     return keywords
-#
-#
-# with codecs.open('keywords.yml', 'r', 'utf-8') as f:
-#     data = yaml.load(f)
-#
-#     if data['groups'] is not None:
-#         for group, keywords in iter(data['groups'].items()):
-#             data['groups'][group] = process_keywords(keywords, False)
-#
-#     if data['triggers'] is not None:
-#         data['triggers'] = process_keywords(data['triggers'])
-#
-#     for condition in data['conditions']:
-#         condition['keywords'] = process_keywords(condition['keywords'])
-#
-#
-# SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
-# sc = SlackClient(SLACK_BOT_TOKEN)
-# twitter = Twitter()
-#
-#
-# def to_pos(sentense):
-#     pos = twitter.pos(sentense, stem=True)
-#     pos = [p[0] for p in pos]
-#     return pos
-#
-#
-# def check_keywords(sentense, keywords, pos=None):
-#     is_valid = True
-#     pos = pos or to_pos(sentense)
-#
-#     for keyword in keywords:
-#         if isinstance(keyword, list):
-#             if any(word in pos for word in keyword):
-#                 continue
-#             if any(word in sentense for word in keyword):
-#                 continue
-#             is_valid = False
-#         else:
-#             if keyword not in pos and keyword not in sentense:
-#                 is_valid = False
-#     return is_valid
-#
-#
-# def find_answer(sentense):
-#     if data['triggers'] is not None:
-#         if not check_keywords(sentense, data['triggers']):
-#             return
-#
-#     pos = to_pos(sentense)
-#     for condition in data['conditions']:
-#         keywords = condition['keywords']
-#         if check_keywords(sentense, keywords, pos=pos):
-#             answers = condition['answers']
-#             if isinstance(answers, list):
-#                 return random.choice(answers)
-#             return answers
-#     return
+    def process_group(self, keyword, allow_group=True):
+        if not keyword.startswith('group:'):
+            return keyword
+
+        if not allow_group:
+            print('group 에서는 group 을 사용할 수 없습니다.')
+            return
+
+        group_key = keyword.split(':')[1]
+        try:
+            return self.groups[group_key]
+        except KeyError:
+            print('groups 에서 {}를 찾을 수 없습니다.', group_key)
+
+    def process_keywords(self, keywords, allow_group=True):
+        if not isinstance(keywords, list):
+            keywords = [keyword.strip() for keyword in keywords.split(',')]
+        keywords = [self.process_group(keyword, allow_group) for keyword in keywords]
+        return keywords
+
+    def load_data(self):
+        with codecs.open('keywords.yml', 'r', 'utf-8') as f:
+            data = yaml.load(f)
+
+            if data['groups'] is not None:
+                for group, keywords in data['groups'].items():
+                    data['groups'][group] = self.process_keywords(keywords, allow_group=False)
+            self.groups = data['groups']
+
+            if data['triggers'] is not None:
+                data['triggers'] = self.process_keywords(data['triggers'])
+            self.triggers = data['triggers']
+
+            for qna in data['qnas']:
+                qna['keywords'] = self.process_keywords(qna['keywords'])
+            self.qnas = data['qnas']
+
+    def to_pos(self, sentence):
+        pos = self.okt.pos(sentence, stem=True)
+        pos = [p[0] for p in pos]
+        return pos
+
+    def check_keywords(self, sentence, keywords, pos=None):
+        is_valid = True
+        pos = pos or self.to_pos(sentence)
+
+        for keyword in keywords:
+            if isinstance(keyword, list):
+                if any(word in pos for word in keyword):
+                    continue
+                if any(word in sentence for word in keyword):
+                    continue
+                is_valid = False
+            else:
+                if keyword not in pos and keyword not in sentence:
+                    is_valid = False
+        return is_valid
+
+    def find_answer(self, sentence):
+        if self.triggers is not None:
+            if not self.check_keywords(sentence, self.triggers):
+                return
+
+        pos = self.to_pos(sentence)
+        for qna in self.qnas:
+            keywords = qna['keywords']
+            if self.check_keywords(sentence, keywords, pos=pos):
+                answers = qna['answers']
+                if isinstance(answers, list):
+                    return random.choice(answers)
+                return answers
